da9e099833acdcb8390f84f666a271a77a4cd0e1
Filipe Cruz <filipemanoeldc@gmail.com>
2025-10-28 19:17:46 -0300

    Refinamento da validação dos campos da tarefa

--- backend/src/task/dto/create-task.dto.ts
+++ backend/src/task/dto/create-task.dto.ts
@@ -1,6 +1,7 @@
-import { IsNotEmpty, IsEmail, MinLength } from 'class-validator';
+import { IsNotEmpty, MaxLength } from 'class-validator';
 
 export class CreateTaskDto {
   @IsNotEmpty({ message: 'O Título é obrigatório' })
+  @MaxLength(150, { message: 'O Título não pode conter mais que 150 caracteres.' })
   title!: string;
 }
